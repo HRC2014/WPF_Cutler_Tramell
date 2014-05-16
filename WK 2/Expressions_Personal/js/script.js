8f5b6abf192d0da1ec9842ff9bbd3fbcf45b7014
HRC2014 <TramellCutler@me.com>
2014-05-15 23:23:07 -0400

    commit
    WPF Assignment

--- WK 2/Expressions_Personal/js/script.js
+++ WK 2/Expressions_Personal/js/script.js
@@ -2,12 +2,14 @@
 //yearBorn
 var state = "1981";
 prompt("What state are you in?");
-console.log(state);
+console.log(N.C.);
 
 //Current Education
 var college = Fullsail
 prompt("What college do you go to?");
-console.log(college);
+console.log(fullsail university);
+
+
 
 
 
