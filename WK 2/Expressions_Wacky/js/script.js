9afc811558b3479bf315fd91d14d6cbffe68f7bd
HRC2014 <TramellCutler@me.com>
2014-05-15 23:28:31 -0400

    commit
    WPF assignment

--- WK 2/Expressions_Wacky/js/script.js
+++ WK 2/Expressions_Wacky/js/script.js
@@ -1,6 +1,6 @@
 //car information
 
-var carModel = "Escalade";
+var carModel = "G 35";
 prompt("What is the model of your car?");
 console.log(carModel);
 
@@ -8,10 +8,10 @@ var carYear = 2008;
 prompt("What year is the year of your car?");
  console.log(carYear);
 
- var carColor = "White"
+ var carColor = "Black"
  prompt("What color is your car?");
  console.log(carColor);
 
- var carMake = "Cadillac"
+ var carMake = "Infinity"
  prompt("What is the make of your car?");
  console.log(carMake);
