2d616f758c7c3e17e4081de1cb025d61e3aa8915
HRC2014 <TramellCutler@me.com>
2014-05-15 23:37:32 -0400

    commit
    wpf assignment

--- WK 2/Expressions_Personal/js/script.js
+++ WK 2/Expressions_Personal/js/script.js
@@ -4,6 +4,10 @@ var city = "Panyego";
 prompt("What state are you in?");
 console.log(city);
 
+var jobOccupation = "Water Treatment Operator";
+prompt("What is your occupation");
+console.log(jobOccupation);
+
 
 
 
