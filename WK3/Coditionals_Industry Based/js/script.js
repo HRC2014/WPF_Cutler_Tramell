8cc851bd946238a71f177d29d84c59c55b511d1d
HRC2014 <TramellCutler@me.com>
2014-05-22 22:14:58 -0400

    commit
    wk3

--- WK3/Coditionals_Industry Based/js/script.js
+++ WK3/Coditionals_Industry Based/js/script.js
@@ -2,6 +2,6 @@ var version = 12;
 
 var macBook;
 
-
+//if students do not have version 12 macBook or above use, other than that do not use
 macBook = (version < 11) ? "Use" : "Do Not Use";
 console.log(macBook);
